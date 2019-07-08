4f8fe30973ab713e47c24ec8c1ac797c43969560
Zolhungaj <26750497+Zolhungaj@users.noreply.github.com>
2019-07-08 23:47:02 +0200

    changed how mediainfo is found
    Because mediainfo is not in my path (rather it is in a bin folder in my path), i upgraded this file to use the config file too. So that I can test things working properly.

--- encoding/autoencoder/autorescheck.py
+++ encoding/autoencoder/autorescheck.py
@@ -3,40 +3,15 @@ Simple piece of code that checks the current video quality
 and then returns the best AMQ option.
 
 Author: FokjeM / RivenSkaye
+        Zolhungaj, upgrade for the addition of a config file
 """
 import os
-import subprocess
+import sys
+import re
 import datetime
 import time
 mediainfo = "mediainfo"
 logfile = "AMQ-autorescheck.log"
-if "\\mediainfo" not in str.lower(os.environ['PATH']):
-    log("MediaInfo not found on path.")
-    try:
-        subprocess.check_call("C:\\Program\ Files\\MediaInfo\\MediaInfo.exe");
-        mediainfo = "C:\\Program\ Files\\MediaInfo\\MediaInfo.exe"
-    except CalledProcessError:
-        log("MediaInfo not installed in Program Files.")
-        try:
-            subprocess.check_call("C:\\Program\ Files\ (x86)\\MediaInfo\\MediaInfo.exe");
-            mediainfo = "C:\\Program\ Files\ (x86)\\MediaInfo\\MediaInfo.exe"
-        except CalledProcessError:
-            log("MediaInfo not found in Program Files (x86).")
-            log("MediaInfo was not found on your system, it is a dependency for this script\r\nGet it at: https://mediaarea.net/en/MediaInfo\r\nIf you already have the CLI version, add it to your PATH.")
-            exit()
-
-def autorescheck(inputfile):
-    print("Using automatic determination for video resolution!")
-    command = '%s "%s" --output=Video;%%Height%%' % (mediainfo, inputfile)
-    process = os.popen(command)
-    height = int(process.read())
-    if height <= 480: # 480 or worse, should be encoded as AMQ 480p
-        return "9"
-    elif height < 720: # This falls under the 576p category, because we know it's more than 480p
-        return "8"
-    else: # It's 720p or better. Someone has quality content!
-        return "7"
-
 def log(message):
     """
     Borrowed Zol's code here
@@ -49,3 +24,25 @@ def log(message):
     file.write(msg)
     file.close()
     print(msg)
+
+with open(os.path.dirname(sys.argv[0]) + os.sep + "autoconvert.config") as file:
+    match = re.search("mediainfo_path" + r"\s?=[ \t\r\f\v]*(.+)$", file.read(), re.I | re.M)
+    if match is None:
+        print("ERROR %s missing in config file" % keyword)
+        input()
+        exit(0)
+    else:
+        mediainfo = match.group(1)
+
+
+def autorescheck(inputfile):
+    print("Using automatic determination for video resolution!")
+    command = '%s "%s" --output=Video;%%Height%%' % (mediainfo, inputfile)
+    process = os.popen(command)
+    height = int(process.read())
+    if height <= 480: # 480 or worse, should be encoded as AMQ 480p
+        return "9"
+    elif height < 720: # This falls under the 576p category, because we know it's more than 480p
+        return "8"
+    else: # It's 720p or better. Someone has quality content!
+        return "7"
