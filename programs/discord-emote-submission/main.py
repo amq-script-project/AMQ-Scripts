5df37a6819afb1af862cc0e5aa3ab238f46ab962
Zolhungaj <26750497+Zolhungaj@users.noreply.github.com>
2019-07-30 00:52:42 +0200

    finished url upload
    duplicated code for url upload

--- programs/discord-emote-submission/main.py
+++ programs/discord-emote-submission/main.py
@@ -1,5 +1,5 @@
 from tkinter.filedialog import askopenfilenames
-from catbox import upload, create_album
+from catbox import upload, create_album, upload_from_url
 import re
 import os
 
@@ -35,13 +35,27 @@ while(True):
     elif selection == "2":
         urls = input("Gib urls, separate them with space\n")
         for url in re.match(r"(?:(.*?)\s)*").groups():
-            ret =  upload_catbox_from_url(url)
+            ret =  upload_from_url(url)
             if ret is not None:
                 file_list.append(ret)
+                prefix, suffix = re.match(r"https://files.catbox.moe/(.+)\.(.+)", ret).groups()
+                scale = 32
+                outputfile = "%s%dx%d.%s" % (prefix, scale, scale, suffix)
+                command  = 'ffmpeg -y -i "%s" ' % f
+                command += '-vf "scale=iw*sar*min(%d/(iw*sar)\,%d/ih):ih*min(%d/(iw*sar)\,%d/ih)' % (scale,)*4  # width:height
+                # ripped from https://superuser.com/questions/891145/ffmpeg-upscale-and-letterbox-a-video#891478
+                command += ', pad=%d:%d:-1:-1:000000@0.0" ' % (scale,)*2  # -1 makes ffmpeg autimatically centre the image, 000000@0.0 is fully transparent black
+                command += '"%s"' % outputfile
+                os.system("start /wait /MIN cmd /c %s" % command)
+                cb = upload(outputfile)
+                small_file_list.append(cb)
     elif selection == "3":
         title = input("provide title\n")
         description = input("explain what's so great about your album\n")
         album = create_album(file_list, title, description)
+        print(album)
+        for link in small_file_list:
+            print(link)
         file_list = []
     elif selection == "4":
         break
