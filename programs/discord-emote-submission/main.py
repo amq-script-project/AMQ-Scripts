007517ee8659a58aa07234e04a3242fcd20581c1
Zolhungaj <26750497+Zolhungaj@users.noreply.github.com>
2019-07-30 00:38:49 +0200

    Update main.py
    implemented ffmpeg command and upload

--- programs/discord-emote-submission/main.py
+++ programs/discord-emote-submission/main.py
@@ -1,7 +1,11 @@
 from tkinter.filedialog import askopenfilenames
+from catbox import upload, create_album
 import re
+import os
+
 
 file_list = []
+small_file_list = []
 while(True):
     selection = input("""what wanna do?
     1: upload more files from computer
@@ -15,6 +19,19 @@ while(True):
             ret =  upload_catbox(f)
             if ret is not None:
                 file_list.append(ret)
+                prefix, suffix = re.match(r"(.+)\.(.+)", f).groups()
+                scale = 32
+                outputfile = "%s%dx%d.%s" % (prefix, scale, scale, suffix)
+                command  = 'ffmpeg -y -i "%s" ' % f
+                command += '-vf "scale=iw*sar*min(%d/(iw*sar)\,%d/ih):ih*min(%d/(iw*sar)\,%d/ih)' % (scale,)*4  # width:height
+                # ripped from https://superuser.com/questions/891145/ffmpeg-upscale-and-letterbox-a-video#891478
+                command += ', pad=%d:%d:-1:-1:000000@0.0" ' % (scale,)*2  # -1 makes ffmpeg autimatically centre the image, 000000@0.0 is fully transparent black
+                command += '"%s"' % outputfile
+                os.system("start /wait /MIN cmd /c %s" % command)
+                cb = upload(outputfile)
+                small_file_list.append(cb)
+            else:
+                print("failed to upload %s" % f)
     elif selection == "2":
         urls = input("Gib urls, separate them with space\n")
         for url in re.match(r"(?:(.*?)\s)*").groups():
@@ -24,7 +41,7 @@ while(True):
     elif selection == "3":
         title = input("provide title\n")
         description = input("explain what's so great about your album\n")
-        create_album(file_list, title, description)
+        album = create_album(file_list, title, description)
         file_list = []
     elif selection == "4":
         break
