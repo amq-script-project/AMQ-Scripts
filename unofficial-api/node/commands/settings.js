5f03004d47870e0ec75dd66308a1a44ce57b39b0
Zolhungaj <26750497+Zolhungaj@users.noreply.github.com>
2020-09-29 01:32:29 +0200

    fixed mysterious game state command

--- unofficial-api/node/commands/settings.js
+++ unofficial-api/node/commands/settings.js
@@ -74,7 +74,7 @@ class Settings extends Commands {
 	}
 
 	sendGameState(inGame, inExpand, inMain) {
-		this._sendCommand({type:"settings",command:"update auto switch avatars", data: {inGame, inExpand, inMain}})
+		this._sendCommand({type:"settings",command:"game state", data: {inGame, inExpand, inMain}})
 	}
 
 	guestRegistration(username, password, email, country) {
