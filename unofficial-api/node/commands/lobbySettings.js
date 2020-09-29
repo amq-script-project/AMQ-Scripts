d526fa2ad43c5d7c23c497a840705e18cb5e7812
Zolhungaj <26750497+Zolhungaj@users.noreply.github.com>
2020-09-30 00:52:35 +0200

    added song count, team size, modifiers, more validation

--- unofficial-api/node/commands/lobbySettings.js
+++ unofficial-api/node/commands/lobbySettings.js
@@ -179,20 +179,24 @@ class LobbySettings{
         this.CONST = {
             ROOM_NAME_MAX_LENGTH:20,
             PASSWORD_MAX_LENGTH:50,
-            ROOM_MIN_SIZE:1,
-            ROOM_MAX_SIZE:40,
+            ROOM_SIZE_MIN:1,
+            ROOM_SIZE_MAX:40,
+            SONG_COUNT_MIN:5,
+            SONG_COUNT_MAX:100,
+            TEAM_SIZE_MIN:1,
+            TEAM_SIZE_MAX:8
         }
         this.oldSettings = JSON.parse(JSON.stringify(this.settings))
     }
     getSettings(){
-        return this.settings
+        return JSON.parse(JSON.stringify(this.settings))
     }
 
     getDelta(){
         const delta = {}
         Object.keys(this.settings).forEach((key) => {
             if(JSON.stringify(this.settings[key]) !== JSON.stringify(this.oldSettings[key])){
-                delta[key] = this.settings[key]
+                delta[key] = JSON.parse(JSON.stringify(this.settings[key]))
             }
         })
         return delta
@@ -205,15 +209,25 @@ class LobbySettings{
     }
 
     setRoomSize(num) {
-        if(num < this.CONST.ROOM_MIN_SIZE || num > this.CONST.ROOM_MAX_SIZE){
-            throw "Room size must be between " + this.CONST.ROOM_MIN_SIZE + " and " + this.CONST.ROOM_MAX_SIZE
+        if(num < this.CONST.ROOM_SIZE_MIN || num > this.CONST.ROOM_SIZE_MAX || !Number.isInteger(num)){
+            throw "Room size must be in the integer interval [" + this.CONST.ROOM_SIZE_MIN + "," + this.CONST.ROOM_SIZE_MAX + "]"
         }
         this.settings.roomSize = num
         gameMode = num > 1 ? "Multiplayer" : "Solo"
     }
 
+    setTeamSize(num) {
+        if(num < this.CONST.TEAM_SIZE_MIN || num > this.CONST.TEAM_SIZE_MAX || !Number.isInteger(num)){
+            throw "Team size must be in the integer interval [" + this.CONST.TEAM_SIZE_MIN + "," + this.CONST.TEAM_SIZE_MAX + "]"
+        }
+        this.settings.teamSize = num
+    }
+
     setRoomName(newName) {
-        if(!newName){
+        if(typeof newName !== "string"){
+            throw "Room name must be string"
+        }
+        if(newName.length === 0){
             throw "Room name cannot be empty"
         }
         if(newName.length > this.CONST.ROOM_NAME_MAX_LENGTH){
@@ -223,17 +237,66 @@ class LobbySettings{
     }
 
     setPassword(newPassword){
+        if(typeof newPassword !== "string"){
+            throw "Password must be string"
+        }
         if(!newPassword){
             return this.clearPassword()
         }
         if(newPassword.length > this.CONST.PASSWORD_MAX_LENGTH){
             throw "Password cannot be longer than " + this.CONST.PASSWORD_MAX_LENGTH + " characters"
         }
+        this.settings.password = newPassword
+        this.settings.privateRoom = true
     }
 
     clearPassword(){
         this.settings.password = ""
         this.settings.privateRoom = false
     }
+
+    setSongCount(num) {
+        if(num < this.CONST.SONG_COUNT_MIN || num > this.CONST.SONG_COUNT_MAX || !Number.isInteger(num)){
+            throw "Song count must be in the integer interval [" + this.CONST.SONG_COUNT_MIN + "," + this.CONST.SONG_COUNT_MAX + "]"
+        }
+        this.settings.numberOfSongs = num
+    }
+
+    setSkipGuessing(bool){
+        if(typeof bool !== "boolean"){
+            throw "skipGuessing must be a bool"
+        }
+        this.settings.modifiers.skipGuessing = bool
+    }
+    
+    setSkipReplay(bool){
+        if(typeof bool !== "boolean"){
+            throw "skipReplay must be a bool"
+        }
+        this.settings.modifiers.skipReplay = bool
+    }
+    
+    setDuplicates(bool){
+        if(typeof bool !== "boolean"){
+            throw "duplicates must be a bool"
+        }
+        this.settings.modifiers.duplicates = bool
+    }
+    
+    setQueueing(bool){
+        if(typeof bool !== "boolean"){
+            throw "queueing must be a bool"
+        }
+        this.settings.modifiers.queueing = bool
+    }
+    
+    setLootDropping(bool){
+        if(typeof bool !== "boolean"){
+            throw "lootDropping must be a bool"
+        }
+        this.settings.modifiers.lootDropping = bool
+    }
+
+    
 }
 module.exports = LobbySettings
