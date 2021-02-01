820b06bc2b32b3730290e99fbb6f3ec1ef4cdb00
Zolhungaj <26750497+Zolhungaj@users.noreply.github.com>
2021-02-01 16:28:36 +0100

    added an utility to get player answer times (by gamePlayerId)

---
+++ gameplay/amqAnswerTimesUtility.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         AMQ Player Answer Times Utility
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Stores time spent answering per player
+// @author       Zolhungaj
+// @match        https://animemusicquiz.com/*
+// @grant        none
+// @downloadURL  https://github.com/amq-script-project/AMQ-Scripts/raw/master/gameplay/amqAnswerTimesUtility.user.js
+// @updateURL    https://github.com/amq-script-project/AMQ-Scripts/raw/master/gameplay/amqAnswerTimesUtility.user.js
+// @copyright    MIT license
+// ==/UserScript==
+
+const amqAnswerTimesUtility = {
+    songStartTime: 0,
+    playerTimes: []
+}
+new Listener("play next song", () => {
+    amqAnswerTimesUtility.songStartTime = Date.now()
+    amqAnswerTimesUtility.playerTimes = []
+}).bindListener()
+
+new Listener("player answered", (data) => {
+    const time = Date.now() - amqAnswerTimesUtility.songStartTime
+    data.forEach(gamePlayerId => {
+        amqAnswerTimesUtility.playerTimes[gamePlayerId] = time
+    })
+}).bindListener()
