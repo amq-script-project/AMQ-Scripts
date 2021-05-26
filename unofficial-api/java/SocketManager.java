07671c6afa5a71afe765e8085a8acbbe7cbd54ca
Zolhungaj <zolhungaj@gmail.com>
2021-05-27 01:26:34 +0200

    now gets cookie had to upgrade to Java 11 to get HTTP2 with the HttpRequest system TODO: see how to switch to HTTPS

--- unofficial-api/java/SocketManager.java
+++ unofficial-api/java/SocketManager.java
@@ -1,41 +1,54 @@
 /* import io.socket.client.IO;
 import io.socket.client.Socket; */
-import java.net.URL;
-import java.net.HttpURLConnection;
-import java.io.OutputStream;
+// import java.net.URL;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Version;
+import java.time.Duration;
+import java.util.List;
+
+// import javax.net.ssl.HttpsURLConnection;
+// import java.io.OutputStream;
 //import java.io.InputStream;
 public class SocketManager{
     public static final String HOST = "animemusicquiz.com";
     public static final String SOCKET_HOST = "https://socket.animemusicquiz.com";
-    public static final URL SIGNIN_URL = new URL("https", HOST, -1, "signIn");
-    public static final URL TOKEN_URL = new URL("https", HOST, -1, "socketToken");
-
-    public SocketManager(String username, String password){
+    public static final URI SIGNIN_URI = URI.create("https://" + HOST + "/signIn");
+    public static final URI TOKEN_URI = URI.create("https://" + HOST + "/socketToken");
+    
+    public SocketManager(String username, String password) throws Exception{
         //get cookie
-        String content = String.format("{'username':'%s','password':'%s'}", username, password);
-        byte[] contentBytes = content.getBytes("UTF-8");
-        HttpURLConnection loginConnection = SIGNIN_URL.openConnection();
-        loginConnection.setRequestMethod("POST");
-        loginConnection.setRequestProperty("Content-Type", "application/json");
-        loginConnection.setRequestProperty("Content-Length", String.valueOf(contentBytes.length));
-        loginConnection.setRequestProperty("Accept", "application/json, text/javascript, */*; q=0.01");
-        loginConnection.setRequestProperty("Accept-Encoding", "gzip, deflate, br");
-        loginConnection.setRequestProperty("Accept-Language", "en-US;q=0.3,en;q=0.1");
-        loginConnection.setRequestProperty("Connection", "keep-alive");
-        loginConnection.setRequestProperty("Host", SIGNIN_URL.getHost());
-        loginConnection.setRequestProperty("Origin", SIGNIN_URL.getProtocol() + "://" + SIGNIN_URL.getHost());
-        loginConnection.setRequestProperty("Referer", SIGNIN_URL.getProtocol() + "://" + SIGNIN_URL.getHost() + "/");
-        loginConnection.setRequestProperty("TE", "Trailers");
-        loginConnection.setRequestProperty("User-Agent", "Mozilla/5.0");
-        loginConnection.setRequestProperty("X-Requested-With", "XMLHttpRequest");
-        loginConnection.setDoInput(true);
-        loginConnection.setDoOutput(true);
-        //InputStream inputStream = loginConnection.getInputStream();
-        OutputStream outputStream = loginConnection.getOutputStream();
-        outputStream.write(contentBytes);
-        String response = loginConnection.getResponseMessage();
+        String content = String.format("{\"username\":\"%s\",\"password\":\"%s\"}", username, password);
+        System.out.println(content);
+        HttpClient client = HttpClient.newBuilder()
+            .version(Version.HTTP_2)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(SIGNIN_URI)
+            .version(Version.HTTP_2)
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json, text/javascript, */*; q=0.01")
+            .header("Accept-Encoding", "gzip, deflate, br")
+            .header("Accept-Language", "en-US;q=0.3,en;q=0.1")
+            // .header("Connection", "keep-alive")
+            // .header("Host", HOST)
+            .header("Origin", "https://" + HOST)
+            .header("Referer", "https://" + HOST + "/")
+            .header("TE", "Trailers")
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0")
+            .header("X-Requested-With", "XMLHttpRequest")
+            .POST(HttpRequest.BodyPublishers.ofString(content))
+            .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        // System.out.println(response.body());
+        HttpHeaders responseHeaders = response.headers();
+        System.out.println(responseHeaders.toString());
+        List<String> cookie = responseHeaders.allValues("set-cookie");
 
-        System.out.println(response);
 /* 
         //SIGNIN_URL
         String cookie = "yadda yadda"; 
@@ -73,7 +86,7 @@ public class SocketManager{
         
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         SocketManager a = new SocketManager(args[0], args[1]);
     }
     
